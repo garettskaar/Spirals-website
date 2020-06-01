44eab90b6abf81b68ec5a3a79d82197350ab9a39
Garett Skaar <skaargarett@gmail.com>
2020-06-01 14:37:57 -0600

    Pushing for remote

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/menus", menus)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, _ *http.Request) {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	handleError(w, err)
+}
+
+func about(w http.ResponseWriter, _ *http.Request) {
+	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
+	handleError(w, err)
+}
+
+func contact(w http.ResponseWriter, _ *http.Request) {
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
+	handleError(w, err)
+}
+
+func menus(w http.ResponseWriter, _ *http.Request) {
+	err := tpl.ExecuteTemplate(w, "menus.gohtml", nil)
+	handleError(w, err)
+}
+
+func handleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
 }
