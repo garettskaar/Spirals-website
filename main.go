847ee86237cd9c1f49034af50bd63d50cce04115
Garett Skaar <skaargarett@gmail.com>
2020-06-05 12:20:04 -0600

    Added variable title and description to head tag

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 
 var tpl *template.Template
 
+type page struct {
+	Title       string
+	Description string
+}
+
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
@@ -26,22 +31,38 @@ func faviconHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func index(w http.ResponseWriter, _ *http.Request) {
-	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	index := page{
+		Title:       "Spirals - Pizza . Sushi",
+		Description: "Spirals pizza happily serves authentic pizza and sushi to the Hiddens Springs community",
+	}
+	err := tpl.ExecuteTemplate(w, "index.gohtml", index)
 	handleError(w, err)
 }
 
 func about(w http.ResponseWriter, _ *http.Request) {
-	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
+	about := page{
+		Title:       "Spirals About",
+		Description: "Spirals is family owned and operated by team Quarles",
+	}
+	err := tpl.ExecuteTemplate(w, "about.gohtml", about)
 	handleError(w, err)
 }
 
 func contact(w http.ResponseWriter, _ *http.Request) {
-	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
+	contact := page{
+		Title:       "Spirals Contact",
+		Description: "Contact Spirals today for pick up, delivery or any questions about the services we offer.",
+	}
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", contact)
 	handleError(w, err)
 }
 
 func menus(w http.ResponseWriter, _ *http.Request) {
-	err := tpl.ExecuteTemplate(w, "menus.gohtml", nil)
+	menus := page{
+		Title:       "Spirals Menu",
+		Description: "Take a look at what delicous food is on Spirals Menu",
+	}
+	err := tpl.ExecuteTemplate(w, "menus.gohtml", menus)
 	handleError(w, err)
 }
 
